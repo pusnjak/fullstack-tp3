bad1614ef3b159a3a7f6f674f773069cc0994b82
unapice <unapice@users.noreply.github.com>
2015-04-15 20:03:55 -0300

    Create fullstack-tp2.js

---
+++ fullstack-tp2.js
@@ -0,0 +1,12 @@
+var salida = salida0 = salida1 = salida2 = "";
+var numero = 100;
+for (i=1; i*i<=numero; i++) { 
+    if (numero % i === 0) {
+        salida1 = salida1 + String(i) + ", ";
+        if (numero !== i*i)
+            salida2 = numero/i + ", " + salida2;
+    }
+}
+salida0 = salida1 + salida2;
+salida = salida0.substring(0, salida0.length - 2);
+console.log(salida);
