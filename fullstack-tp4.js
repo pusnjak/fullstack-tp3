3505824ad830d95c4eac5cf036a92ffffa27d291
UnApice <unapice@yopmail.com>
2015-04-27 20:24:44 -0300

    agregado tp4

---
+++ fullstack-tp4.js
@@ -0,0 +1,18 @@
+var entrada = ["palabra", "mayúscula", "estrafalario", "corta"];
+var vocales = "aeiouáéíóúü";
+
+var resultado = [];
+
+for (var i=0; i<entrada.length; i++) {
+    var cuantas = 0;
+    for (var j=0; j<entrada[i].length; j++) {
+        indice = vocales.indexOf(entrada[i].substr(j,1).toLowerCase());
+        // resultado.push(indice);
+        if (indice >= 0) {
+            cuantas++;
+        }
+    }
+    resultado.push(cuantas);
+}
+
+console.log(resultado);
